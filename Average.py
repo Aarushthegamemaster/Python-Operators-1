efd7c4753a636974d2f48d7320517f80cd2b09ac
Aarushthegamemaster <aarushchandra0604@gmail.com>
2025-02-22 10:28:21 -0800

    Averages_are_sick

---
+++ Average.py
@@ -0,0 +1,11 @@
+age1 = 46
+age2 = 44
+age3 = 11
+age4 = 28
+age5 = 25
+
+sum_of_num = age1+age2+age3+age4+age5
+print("The sum of each of these ages is", sum_of_num)
+
+mean = sum_of_num/5
+print("the Mean of these numbers is", mean)
